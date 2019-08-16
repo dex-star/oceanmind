522081fdc43cfde9b9abb0463c7a6974248105fb
dex-star <53912682+dex-star@users.noreply.github.com>
2019-08-16 12:51:30 -0500

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -1,6 +1,6 @@
 
 <?php
-	$mysqli = new mysqli('oceanmind.com.mx', 'oceanminduser', 'oceanmindteam', 'oceanmind');
+	$mysqli = new mysqli('oceanmind.com.mx', 'ocea', '12345', 'oceanmind');
 	if($mysqli->connect_errno):
 		echo "Error al conectarse con MySQL debido al error" .$mysqli->connect_error;
 endif;
