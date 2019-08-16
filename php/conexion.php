c18315d31ac7e56b1a146c40c07c8b004342b6ab
dex-star <53912682+dex-star@users.noreply.github.com>
2019-08-16 12:40:07 -0500

    conexion

--- php/conexion.php
+++ php/conexion.php
@@ -1,6 +1,6 @@
 
 <?php
-	$mysqli = new mysqli('redgeek.online', 'u682599546_om', 'RcelJCUm6lHD', 'u682599546_bd');
+	$mysqli = new mysqli('oceanmind.com.mx', 'oceanminduser', 'oceanrules', 'oceanmind');
 	if($mysqli->connect_errno):
 		echo "Error al conectarse con MySQL debido al error" .$mysqli->connect_error;
 endif;
